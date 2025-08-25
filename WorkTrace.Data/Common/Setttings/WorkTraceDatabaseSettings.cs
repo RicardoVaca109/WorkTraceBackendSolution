a7b8561b97953e242d38ef27e5380f80f294f9aa
Ricardo <ricardo.vaca@udla.edu.ec>
2025-08-25 08:12:36 -0500

    Database Context is Added

---
+++ WorkTrace.Data/Common/Setttings/WorkTraceDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿
+namespace WorkTrace.Data.Common.Setttings
+{
+    public class WorkTraceDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DataBaseName { get; set; } = null!;
+    }
+}
---
+++ WorkTrace.Data/WorkTraceContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using WorkTrace.Data.Common.Setttings;
+
+namespace WorkTrace.Data
+{
+    public class WorkTraceContext
+    {
+        private readonly MongoClient _mongoClient;
+        private readonly IMongoDatabase _database;
+        public WorkTraceContext(IOptions<WorkTraceDatabaseSettings> workTraceDatabaseSettings) 
+        {
+            _mongoClient = new MongoClient(workTraceDatabaseSettings.Value.ConnectionString);
+
+           _database = _mongoClient.GetDatabase(workTraceDatabaseSettings.Value.DataBaseName);
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string collectionName) 
+        {
+            return _database.GetCollection<T>(collectionName);  
+        }
+    }
+}
