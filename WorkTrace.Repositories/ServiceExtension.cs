4a62769b20a05d1ce15445308584b47710979e86
Ricardo <ricardo.vaca@udla.edu.ec>
2025-09-10 23:37:17 -0500

    feat: Service Extension Dependency Injection

---
+++ WorkTrace.Repositories/ServiceExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WorkTrace.Application.Repositories;
+using WorkTrace.Repositories.Repositories;
+
+namespace WorkTrace.Repositories;
+
+public static class ServiceExtension
+{
+    public static void AddRepositoriesServices(this IServiceCollection services)
+    {
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IStatusRepository, StatusRepository>();
+        services.AddScoped<IServiceRepository, ServiceRepository>();
+
+    }
+}
