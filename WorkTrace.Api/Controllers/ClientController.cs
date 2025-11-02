d02a6db6b3234d7ab3cfe37b95ec9eb7fc103be3
Ricardo <ricardo.vaca@udla.edu.ec>
2025-11-01 21:19:12 -0500

    style: Fix interface variable connection

--- WorkTrace.Api/Controllers/ClientController.cs
+++ WorkTrace.Api/Controllers/ClientController.cs
@@ -1,28 +1,26 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WorkTrace.Application.DTOs.ClientDTO.Information;
 using WorkTrace.Application.Services;
 
-namespace WorkTrace.Api.Controllers
+namespace WorkTrace.Api.Controllers;
+
+[Route("[controller]/[action]")]
+[ApiController]
+public class ClientController(IClientService clientService) : ControllerBase
 {
-    [Route("[controller]/[action]")]
-    [ApiController]
-    public class ClientController(IClientService _clientService) : ControllerBase
-    {
-        [Authorize]
-        [HttpGet]
-        public async Task<List<ClientInformationResponse>> GetAll() =>
-            await _clientService.GetAllAsync();
+    [Authorize]
+    [HttpGet]
+    public async Task<List<ClientInformationResponse>> GetAll() =>
+        await clientService.GetAllAsync();
 
-        [Authorize]
-        [HttpPost]
-        public async Task<ClientInformationResponse> Create(CreateClientRequest request) =>
-            await _clientService.CreateClientAsync(request);
+    [Authorize]
+    [HttpPost]
+    public async Task<ClientInformationResponse> Create(CreateClientRequest request) =>
+        await clientService.CreateClientAsync(request);
 
-        [Authorize]
-        [HttpPut]
-        public async Task<ClientInformationResponse> Update(string id, UpdateClientRequest request) =>
-            await _clientService.UpdateClientAsync(id, request);
-    }
+    [Authorize]
+    [HttpPut]
+    public async Task<ClientInformationResponse> Update(string id, UpdateClientRequest request) =>
+        await clientService.UpdateClientAsync(id, request);
 }
