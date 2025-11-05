3808beeb94c8d5226ecb9b9c943a13759bc86dd6
Ricardo <ricardo.vaca@udla.edu.ec>
2025-11-04 23:06:06 -0500

    feat: Possible Assigment embedded models for logic

---
+++ WorkTrace.Data/Models/StepProgress.cs
@@ -0,0 +1,8 @@
+﻿using WorkTrace.Data.Common.Generics;
+
+namespace WorkTrace.Data.Models;
+public class StepProgress : BaseModel
+{
+    public bool IsCompleted { get; set; }
+    public DateTime? CompletedAt { get; set; }
+}
