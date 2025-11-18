7ecbeb98699a7704be3b04cdfbb801817e5bf3c0
Ricardo <ricardo.vaca@udla.edu.ec>
2025-11-17 23:24:14 -0500

    feat: Solution change name and delete of web app layer

--- WorkTrace.WebApp/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace WorkTrace.WebApp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
