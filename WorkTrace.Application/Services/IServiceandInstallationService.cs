88b71ea1aed43119f29108ddda56f472bd24511a
Ricardo <ricardo.vaca@udla.edu.ec>
2025-11-18 18:51:02 -0500

    feat: Add GtebyId Interface Logic

--- WorkTrace.Application/Services/IServiceandInstallationService.cs
+++ WorkTrace.Application/Services/IServiceandInstallationService.cs
@@ -5,7 +5,7 @@ namespace WorkTrace.Application.Services;
 public interface IServiceandInstallationService
 {
     Task<List<ServiceInformationResponse>> GetAllAsync();
-    Task<ServiceInformationResponse?> GetByIdAsync(string id);
+    Task<ServiceInformationResponse> GetByIdAsync(string id);
     Task<ServiceInformationResponse> CreateServiceWithStepAsync(CreateServiceRequest request);
     Task<ServiceInformationResponse> UpdateServiceAsync(string id, UpdateServiceRequest request);
 }
