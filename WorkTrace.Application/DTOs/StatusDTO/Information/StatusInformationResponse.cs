12f9f64560554911594d1ce7f30bd95574eda6bf
Ricardo <ricardo.vaca@udla.edu.ec>
2025-11-17 23:29:26 -0500

    fix: Implementing ID in dtos Response

--- WorkTrace.Application/DTOs/StatusDTO/Information/StatusInformationResponse.cs
+++ WorkTrace.Application/DTOs/StatusDTO/Information/StatusInformationResponse.cs
@@ -2,6 +2,7 @@
 
 public class StatusInformationResponse
 {
+    public string Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public bool IsActive { get; set; }
