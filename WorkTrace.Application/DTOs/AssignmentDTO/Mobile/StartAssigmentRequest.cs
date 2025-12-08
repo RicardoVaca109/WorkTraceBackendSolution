399b2aae0b54f9cf705d321062869c4eebf4aa70
CrisJurado10 <juradoc345@gmail.com>
2025-12-08 00:41:30 -0500

    ups

--- WorkTrace.Application/DTOs/AssignmentDTO/Mobile/StartAssigmentRequest.cs
+++ WorkTrace.Application/DTOs/AssignmentDTO/Mobile/StartAssigmentRequest.cs
@@ -1,9 +1,13 @@
-﻿using WorkTrace.Data.Models;
+﻿using System.Text.Json.Serialization;
+using WorkTrace.Data.Models;
 
 namespace WorkTrace.Application.DTOs.AssignmentDTO.Mobile;
 
 public class StartAssigmentRequest
 {
+    [JsonPropertyName("checkIn")]
     public DateTime CheckIn { get; set; }
+
+    [JsonPropertyName("currentLocation")]
     public GeoPoint CurrentLocation { get; set; }
 }
--- WorkTrace.Data/Models/GeoPoint.cs
+++ WorkTrace.Data/Models/GeoPoint.cs
@@ -1,8 +1,15 @@
-﻿namespace WorkTrace.Data.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace WorkTrace.Data.Models;
 
 public class GeoPoint
 {
+    [JsonPropertyName("latitude")]
     public double Latitude { get; set; }
+
+    [JsonPropertyName("longitude")]
     public double Longitude { get; set; }
+
+    [JsonPropertyName("updatedAt")]
     public DateTime UpdatedAt { get; set; }
 }
