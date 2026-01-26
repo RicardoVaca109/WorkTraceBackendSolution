fa9f5ff6ff8c68ca45d89992ab0b1fb2105c4935
Ricardo <ricardo.vaca@udla.edu.ec>
2026-01-26 01:24:01 -0500

    feat: ? in Id for UpdateFormQuestionsRequest

--- WorkTrace.Application/DTOs/FormTemplateDTO/Information/UpdateFormQuestionsRequest.cs
+++ WorkTrace.Application/DTOs/FormTemplateDTO/Information/UpdateFormQuestionsRequest.cs
@@ -4,7 +4,7 @@ namespace WorkTrace.Application.DTOs.FormTemplateDTO.Information;
 
 public class UpdateFormQuestionsRequest
 {
-    public string Id { get; set; }
+    public string? Id { get; set; }
     public string? QuestionKey { get; set; }
     public string? QuestionText { get; set; }
     public AnswerType? AnswerType { get; set; }
